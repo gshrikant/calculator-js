92da1b5a306daa0d9a4197711c689371e071bcb8
Shrikant Giridhar <shrikantgiridhar@gmail.com>
2018-06-22 03:30:59 -0400

    Add keyboard text entry support.
    Users can now press keys to enter numbers (wow!). The individual
    letters are also non-selectable now which makes them feel more
    real Web 2.0 buttons.

--- calc.js
+++ calc.js
@@ -1,16 +1,20 @@
 // Clippy Calculator
+const allowedKeys = "0123456789/*+-c=";
+
 var textField = document.getElementById("display");
 var buttonList = document.getElementsByClassName("button");
 var enteredValues = "";
 
-// Clear text input field on window load.
 window.onLoad = function() {
+    // Clear text input field on window load.
     textField = "";
 }
 
+// Setup event callbacks for all input elements.
 for (let b = 0; b < buttonList.length; ++b) {
     var oldColor = buttonList[b].style.backgroundColor;
 
+    // Highlight on mouse hover.
     buttonList[b].addEventListener("mouseenter", function(event) {
         event.target.style.backgroundColor = "rgba(100, 100, 100, 0.25)";
     }, false);
@@ -19,6 +23,7 @@ for (let b = 0; b < buttonList.length; ++b) {
         event.target.style.backgroundColor = oldColor;
     }, false);
 
+    // Deepen color on click for user feedback.
     buttonList[b].addEventListener("click", function(event) {
         event.target.style.backgroundColor = "gray";
         let buttonText = buttonList[b].innerHTML;
@@ -28,9 +33,24 @@ for (let b = 0; b < buttonList.length; ++b) {
             enteredValues = "";
             textField.value = "";
             return;
+        } else if (buttonId == "eval") {
+            return;
         }
 
         enteredValues += buttonText;
         textField.value = enteredValues;
     }, false);
+
 }
+
+// Allow keyboard input for selected keys.
+document.addEventListener("keydown", function(event) {
+    if (allowedKeys.includes(event.key)) {
+        console.log("Pressed: " + event.key);
+        if (event.key != "c" && event.key != "=") {
+            textField.focus();
+        } else {
+            textField.blur();
+        }
+    }
+}, false);
